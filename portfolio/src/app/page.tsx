9d314bfce967905841bb9cb0656ec7840c766e47
RheyanJohn <rjvblanco.chmsu@gmail.com>
2025-01-08 14:03:51 +0800

    Add Some info

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <h1 className="text-6xl text-white font-bold">Hello i am Rheyan John</h1>
       <h1 className="text-6xl text-white font-bold">a Full Stack Web Developer</h1>
+      <p className="text-2xl text-white">First Commit Development Underway</p>
     </div>
   );
 }
