783aef1fa1e40e3c59d20ed2f611e1d3e3a01f51
RheyanJohn <rjvblanco.chmsu@gmail.com>
2025-01-08 13:59:46 +0800

    First Commit

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <h1 className="text-6xl text-white font-bold">Hello i am Rheyan John</h1>
+      <h1 className="text-6xl text-white font-bold">a Full Stack Web Developer</h1>
+    </div>
+  );
+}
