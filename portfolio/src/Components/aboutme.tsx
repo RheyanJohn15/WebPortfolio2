d2b80c0aa9b857508fb86b4217464766c9eaa8c3
RheyanJohn <rjvblanco.chmsu@gmail.com>
2025-01-13 14:28:06 +0800

    About Me Page

--- portfolio/src/Components/aboutme.tsx
+++ portfolio/src/Components/aboutme.tsx
@@ -1,16 +1,53 @@
 import BoxReveal from "./ui/box-reveal"
+import Image from "next/image"
+import React from "react";
+import { textVal } from "@/data/text";
 
 export default function AboutMe() {
+   
     return (
-        <div className="w-full h-screen grid grid-cols-3">
-          <div className="col-span-2 p-24 border-2 border-sky-700">
-            <h1 className="text-black"> Hello World</h1>
+        <div className="w-full grid grid-cols-2 px-24">
+          <div className="col-span-1">
             <BoxReveal boxColor={"#000"} duration={0.5}>
-                <p className="text-[3.5rem] font-semibold">
-                    About Me
+                <p className="text-6xl mb-8 font-semibold">
+                    {textVal.aboutme.header}
+                </p>
+            </BoxReveal>
+
+            <BoxReveal boxColor={"#000"} duration={0.5}>
+                <p className="mb-4">
+                    {textVal.aboutme.desc1}
+                </p>
+            </BoxReveal>
+
+            <BoxReveal boxColor={"#000"} duration={0.5}>
+                <p className="mb-4">
+                    {textVal.aboutme.desc2}
+                </p>
+            </BoxReveal>
+
+            <BoxReveal boxColor={"#000"} duration={0.5}>
+                <p className="mb-4">
+                    {textVal.aboutme.desc3}
+                </p>
+            </BoxReveal>
+
+            <BoxReveal boxColor={"#000"} duration={0.5}>
+                <p>
+                    {textVal.aboutme.desc4}
+                </p>
+            </BoxReveal>
+
+            <BoxReveal boxColor={"#000"} duration={0.5}>
+                <p>
+                    {textVal.aboutme.desc5}
                 </p>
             </BoxReveal>
           </div>
+
+          <div className="col-span-1 flex justify-center">
+              <Image src="/portf/me.jpg" className="rounded-full w-[24rem] h-[24rem] " width={500} height={500} alt="Rheyan John Blanco" />
+          </div>  
         </div>
     )
 }
---
+++ portfolio/src/data/text.tsx
@@ -0,0 +1,16 @@
+export const textVal = {
+    name: "Rheyan John Blanco",
+    intro1: "Hi There 👋",
+    intro2: "I am Rheyan John",
+    intro3: "a Full stack web developer",
+    seeMore: "See More",
+
+    aboutme:{
+        header: "About Me",
+        desc1: "I am a passionate and adaptable full-stack web developer with expertise in a diverse range of technologies, including strong proficiency in back-end frameworks like Laravel and Django, and full-stack development using Next.js. My solid foundation in programming concepts and fundamentals enables me to craft efficient, scalable, and user-friendly applications.",
+        desc2: "I pride myself on adhering to industry best practices, maintaining clean and maintainable code, and following coding standards and conventions. This disciplined approach ensures my work is reliable, easy to build upon, and fosters seamless collaboration within teams.",
+        desc3: "Beyond technical expertise, I excel at problem-solving and have a proven ability to break down complex challenges into actionable solutions. My strong communication and teamwork skills allow me to contribute meaningfully to a collaborative environment, whether leading initiatives or supporting others to achieve shared goals.",
+        desc4: "What sets me apart is my ability to quickly adapt to new frameworks, languages, and tools. I am committed to continuous learning and staying ahead in the ever-evolving tech landscape, consistently delivering solutions that exceed expectations.",
+        desc5: "From building dynamic, responsive user interfaces to designing robust and secure back-end systems, I thrive in environments that challenge me to innovate and grow. I am passionate about creating impactful software and contributing to a positive and productive team culture.",
+    }
+  };
