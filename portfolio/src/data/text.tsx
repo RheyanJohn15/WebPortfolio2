f81f1a6981ac4b88facf1b351fd2588391790719
RheyanJohn15 <rjvblanco.chmsu@gmail.com>
2025-04-29 20:09:19 -1200

    adjust about me

--- portfolio/src/data/text.tsx
+++ portfolio/src/data/text.tsx
@@ -31,7 +31,7 @@ export const textVal = {
 
     aboutme:{
         header: "About Me",
-        desc1: "I am a passionate and adaptable full-stack web developer with expertise in a diverse range of technologies, including strong proficiency in back-end frameworks like Laravel and Django, and full-stack development using Next.js. My solid foundation in programming concepts and fundamentals enables me to craft efficient, scalable, and user-friendly applications.",
+        desc1: "I am a passionate and adaptable full-stack web developer with expertise in a diverse range of technologies, including strong proficiency in back-end frameworks like Laravel and Django, and full-stack development using Next.js and VueJs. My solid foundation in programming concepts and fundamentals enables me to craft efficient, scalable, and user-friendly applications.",
         desc2: "I pride myself on adhering to industry best practices, maintaining clean and maintainable code, and following coding standards and conventions. This disciplined approach ensures my work is reliable, easy to build upon, and fosters seamless collaboration within teams.",
         desc3: "Beyond technical expertise, I excel at problem-solving and have a proven ability to break down complex challenges into actionable solutions. My strong communication and teamwork skills allow me to contribute meaningfully to a collaborative environment, whether leading initiatives or supporting others to achieve shared goals.",
         desc4: "What sets me apart is my ability to quickly adapt to new frameworks, languages, and tools. I am committed to continuous learning and staying ahead in the ever-evolving tech landscape, consistently delivering solutions that exceed expectations.",
